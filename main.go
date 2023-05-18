56baeea5f00f4a4b7a78fa0bf860a8dc2e101043
Dylan Moreland <dylan@dimo.zone>
2023-05-18 11:21:38 -0400

    Full loop

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -12,7 +14,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func client(cid, port uint32) {
+func client(cid, port uint32, c cred) {
 	fd, err := unix.Socket(unix.AF_VSOCK, unix.SOCK_STREAM, 0)
 	if err != nil {
 		panic(err)
@@ -27,7 +29,15 @@ func client(cid, port uint32) {
 		panic(err)
 	}
 
-	if err := unix.Send(fd, []byte("Hello, world!"), 0); err != nil {
+	m := msg{
+		AccessKeyID:     c.AccessKeyID,
+		SecretAccessKey: c.SecretAccessKey,
+		Token:           c.Token,
+	}
+
+	b, _ := json.Marshal(m)
+
+	if err := unix.Send(fd, b, 0); err != nil {
 		panic(err)
 	}
 }
@@ -36,6 +46,12 @@ const (
 	backlog = 128
 	bufsize = 1024
 )
+
+type cred struct {
+	AccessKeyID     string `json:"AccessKeyId"`
+	SecretAccessKey string `json:"SecretAccessKey"`
+	Token           string `json:"Token"`
+}
 
 func server(port uint32) {
 	fd, err := unix.Socket(unix.AF_VSOCK, unix.SOCK_STREAM, 0)
@@ -77,21 +93,40 @@ func server(port uint32) {
 				break
 			}
 
-			/*
-							           "/kmstool_enclave_cli",
-				           "decrypt",
-				           "--region", "us-east-1",
-				           "--proxy-port", "8000",
-				           "--aws-access-key-id", access_key_id,
-				           "--aws-secret-access-key", secret_access_key,
-				           "--aws-session-token", token,
-				           "--ciphertext", ciphertext,
-			*/
-			os.Open("")
+			log.Printf("Got message: %s", string(buf[:n]))
 
-			log.Printf("Got message: %s.", string(buf[:n]))
+			var m msg
+			if err := json.Unmarshal(buf[:n], &m); err != nil {
+				log.Printf("Failed to unmarshal message: %s", err)
+				break
+			}
+
+			cmd := exec.Command(
+				"/kmstool_enclave_cli",
+				"decrypt",
+				"--region", "us-east-2",
+				"--proxy-port", "8000",
+				"--aws-access-key-id", m.AccessKeyID,
+				"--aws-secret-access-key", m.SecretAccessKey,
+				"--aws-session-token", m.Token,
+				"--ciphertext", m.Ciphertext,
+			)
+
+			out, err := cmd.Output()
+			if err != nil {
+				log.Printf("Failed executing KMS command: %s", err)
+			}
+
+			log.Printf("Got message: %s.", string(out))
 		}
 	}
+}
+
+type msg struct {
+	AccessKeyID     string `json:"accessKeyId"`
+	SecretAccessKey string `json:"secretAccessKey"`
+	Token           string `json:"token"`
+	Ciphertext      string `json:"ciphertext"`
 }
 
 func main() {
@@ -109,11 +144,12 @@ func main() {
 			return
 		}
 
-		client := imds.NewFromConfig(cfg)
-		mo, err := client.GetMetadata(ctx, &imds.GetMetadataInput{Path: "iam/security-credentials/dev-ec2-test-enclave"})
+		md := imds.NewFromConfig(cfg)
+		mo, err := md.GetMetadata(ctx, &imds.GetMetadataInput{Path: "iam/security-credentials/dev-ec2-test-enclave"})
 		if err != nil {
 			panic(err)
 		}
+
 		defer mo.Content.Close()
 
 		b, err := io.ReadAll(mo.Content)
@@ -121,21 +157,24 @@ func main() {
 			panic(err)
 		}
 
-		log.Println("MO", string(b))
+		var c cred
+		if err := json.Unmarshal(b, &c); err != nil {
+			panic(err)
+		}
 
-		// a := os.Args[2:]
-		// if len(a) != 2 {
-		// 	panic("cid and port arguments required")
-		// }
-		// cid, err := strconv.ParseUint(a[0], 10, 32)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// port, err := strconv.ParseUint(a[1], 10, 32)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// client(uint32(cid), uint32(port))
+		a := os.Args[2:]
+		if len(a) != 2 {
+			panic("cid and port arguments required")
+		}
+		cid, err := strconv.ParseUint(a[0], 10, 32)
+		if err != nil {
+			panic(err)
+		}
+		port, err := strconv.ParseUint(a[1], 10, 32)
+		if err != nil {
+			panic(err)
+		}
+		client(uint32(cid), uint32(port))
 	case "server":
 		a := os.Args[2:]
 		if len(a) != 1 {
