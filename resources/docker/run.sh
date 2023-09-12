d35a19308ee0e4e9e4c4e68253e93dcbd38bc8d3
Yevgeny Khessin <yev@dimo.zone>
2023-09-12 12:03:09 -0400

    Expanding workflow to download artifacts

--- resources/docker/run.sh
+++ resources/docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-readonly EIF_PATH="/synthetic-wallet-enclave.eif"
+readonly EIF_PATH="/eif/synthetic-wallet-enclave.eif"
 readonly ENCLAVE_CPU_COUNT=2
 readonly ENCLAVE_MEMORY_SIZE=1024
 readonly ENCLAVE_CID=16
@@ -13,6 +13,7 @@ main() {
     /nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
         --eif-path $EIF_PATH --debug-mode --enclave-cid $ENCLAVE_CID
 
+    vsock-proxy 8000 kms.$AWS_REGION.amazonaws.com 443 &
     sleep infinity
 }
 
