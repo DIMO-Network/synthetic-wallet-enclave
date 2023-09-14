843daccd780aa07bd780d9d6b43ce9c6d09fe5a3
Yevgeny Khessin <yev@dimo.zone>
2023-09-14 02:25:40 -0400

    Adding enclave run commands

--- resources/docker/run.sh
+++ resources/docker/run.sh
@@ -1,42 +1,48 @@
 #!/bin/bash -e
-# Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 set -x	
+readonly ENCLAVE_NAME="synthetic-wallet-enclave"
 readonly EIF_PATH="/eif/synthetic-wallet-enclave.eif"
 
-if [[ -z "${CPU_LIMIT}" ]]; then
-  ENCLAVE_CPU_COUNT=2
-else
-  ENCLAVE_CPU_COUNT="${CPU_LIMIT}"
-fi
 
-if [[ -z "${HUGEPAGES_LIMIT}" ]]; then
-  ENCLAVE_MEMORY_SIZE=512
-else
-  ENCLAVE_MEMORY_SIZE="${HUGEPAGES_LIMIT}"
-fi
+ENCLAVE_CPU_COUNT=${ENCLAVE_CPU_COUNT:-1}
+ENCLAVE_MEMORY_SIZE=${ENCLAVE_MEMORY_SIZE:-768}
+ENCLAVE_CID=${ENCLAVE_CID:-16}
+AWS_REGION=${AWS_REGION:-us-east-1}
 
-if [[ -z "${AWS_REGION}" ]]; then
-  AWS_REGION=us-east-1
-else
-  AWS_REGION="${AWS_REGION}"
-fi
-
-if [[ -z "${ENCLAVE_CID}" ]]; then
-  ENCLAVE_CID=16
-else
-  ENCLAVE_CID="${ENCLAVE_CID}"
-fi
-
-main() {
-    nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
-        --eif-path $EIF_PATH --debug-mode --enclave-cid $ENCLAVE_CID
-
-    vsock-proxy 8000 kms.$AWS_REGION.amazonaws.com 443 &
-    sleep infinity
+term_handler() {
+  echo 'Shutting down enclave'
+  nitro-cli terminate-enclave --enclave-name $ENCLAVE_NAME
+  kill -SIGTERM $(pgrep nitro-cli)
+  kill -SIGTERM $(pgrep vsock-proxy)
+  echo 'Shutdown complete'
+  exit 0;
 }
 
-main
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
 
-#nitro-cli terminate-enclave --all
-#nitro-cli describe-enclaves | jq -r .[0].EnclaveID
+# run application
+
+vsock-proxy 8000 kms.$AWS_REGION.amazonaws.com 443 &
+
+nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
+    --eif-path $EIF_PATH --enclave-cid $ENCLAVE_CID --attach-console &
+
+if [[ ! -v "${ENCLAVE_DEBUG_MODE}" ]]; then
+  echo 'Starting production enclave.'
+  nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
+    --eif-path $EIF_PATH --enclave-cid $ENCLAVE_CID &
+  echo 'Enclave running.'
+else
+  echo 'Starting development enclave.'
+  nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
+    --eif-path $EIF_PATH --enclave-cid $ENCLAVE_CID --attach-console &
+  echo 'Enclave started in debug mode.'
+fi
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
